f05b55d9ef997fcd84db7f4191d2c62aa2019fd1
Student <example@example.com>
2019-11-06 15:44:05 -0800

    create html grid, link with js grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,90 @@
 var spacesArray = [[1,2,3],[4,5,6],[7,8,9]];
 
-function Board() {
+function Board(p1, p2) {
   this.grid = [["","",""],["","",""],["","",""]];
-  this.turn = 0;
-  this.currentPlayer = 0;
-
+  this.turn = 1;
+  this.currentPlayer = p1;
+  this.players = [p1, p2]
 }
-Board.prototype.checkStatus = function() {
+  Board.prototype.checkStatus = function() {
+
   //check if the board has winning moves
+  var xDiag = 0;
+  var oDiag = 0;
   for (var j=0; j<3; j++) {
     var xCount = 0;
     var oCount = 0;
+
     for (var i=0; i<3; i++){
-      //Check Vertical Wins
+
+      //Check Negative Diagonal Win
+      if (i + j == 2 && this.grid[j][i] === "X") {
+        xDiag += 1;
+        if (xDiag === 3) { console.log('Negative Diagonal X'); return "Player 1 wins";}
+      } else if (i + j == 2 && this.grid[j][i] === "O") {
+        oDiag += 1;
+        if (oDiag === 3) { console.log('Negative Diagonal O'); return "Player 2 wins"}
+      }
+      //Check Horizontal Wins
       if (this.grid[j][i] === "X"){
         xCount += 1;
-        if (xCount === 3) {return "Player 1 wins"}
+        if (xCount === 3) { console.log('Horizontal X'); return "Player 1 wins";}
       } else if (this.grid[j][i] === "O") {
         oCount += 1;
-        if (oCount === 3) {return "Player 2 wins"}
+        if (oCount === 3) { console.log('Horizontal O'); return "Player 2 wins";}
       }
     }
-    //Check Horizontal Wins
+    //Check Vertical Wins
     xCount = 0;
     oCount = 0;
     for (var i=0; i<3; i++){
       if (this.grid[i][j] === "X"){
         xCount += 1;
-        if (xCount === 3) {return "Player 1 wins"}
+        if (xCount === 3) { console.log('Vertical X'); return "Player 1 wins";}
       } else if (this.grid[i][j] === "O") {
         oCount += 1;
-        if (oCount === 3) {return "Player 2 wins"}
+        if (oCount === 3) { console.log('Vertical O'); return "Player 2 wins";}
       }
     }
   }
+  //Check Positive Diagonal Win
+  var xCount = 0;
+  var oCount = 0;
   for (var j=0; j<3; j++) {
-    var xCount = 0;
-    var oCount = 0;
     if (this.grid[j][j] === 'X') {
       xCount += 1;
-      if (xCount == 3) {return "Player 1 wins"}
+      if (xCount === 3) { console.log('Positive Diagonal X'); return "Player 1 wins";}
     } else if (this.grid[j][j] === "O") {
       oCount += 1;
-      if (oCount === 3) {return "Player 2 wins"}
+      if (oCount === 3) { console.log('Positive Diagonal O'); return "Player 2 wins";}
     }
   }
-  //Diagonal
-  //x=y
-  //x+y=2
   //Check Draw
+  if (this.turn>9){
+    console.log("draw = you all lose");
+  }
 }
 
 Board.prototype.advanceTurn = function() {
-  this.turn += 1
-  if (turn % 2 === 0) {       //Determine Player
-    this.currentPlayer = 1;
+  if (this.turn % 2 == 0) {       //Switch Players
+    this.currentPlayer = this.players[0];
   } else {
-    this.currentPlayer = 2
+    this.currentPlayer = this.players[1];
   }
+  this.turn += 1
   return
 }
 
-Board.prototype.displayBoard = function() {
-  //output board to html
+Board.prototype.makeMove = function(row,col) {
+  if (!this.grid[row][col]) {
+    this.grid[row][col] = this.currentPlayer.mark;
+    console.log('success')
+    this.advanceTurn();
+
+  } else {
+    //return an error and alert user
+    console.log('space taken')
+  }
 }
 
 function Player(id) {
@@ -76,32 +98,27 @@ function Player(id) {
   this.loss = 0;
 
 }
-
-Player.prototype.makeMove = function(x,y,board) {
-  if (!board.grid[x][y]) {
-    board.grid[x][y] = this.mark;
-    console.log('success')
-    board.advanceTurn(this);
-    return true;
-  } else {
-    //return an error and alert user
-    console.log('space taken')
-  }
+Player.prototype.checkPlayerId = function(id) {
+  if (this.id == id);
+  return true
 }
 
-Player.prototype.checkPlayer = function() {
-  return this.id;
-}
-
-//test objects
-testBoard = new Board();
-testPlayer = new Player(1);
+player1 = new Player(1);
+player2 = new Player(2);
+gameBoard = new Board(player1, player2);
 
 
 
 $(document).ready(function(){
-
-  if (testPlayer.makeMove())
-
+  $('.card').click(function(){
+    space = this['id'];
+    space = space.split(',');
+    row = parseInt(space[0]);
+    col = parseInt(space[1]);
+    console.log(row);
+    console.log(col);
+    gameBoard.makeMove(row,col)
+    console.log(gameBoard)
+  })
 
 });

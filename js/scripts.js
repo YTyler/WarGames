d279c4776ca5d9e326e53f71a373aa2afcdf7307
Student <example@example.com>
2019-11-06 11:03:32 -0800

    create semifunctional business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,86 @@ var spacesArray = [[1,2,3],[4,5,6],[7,8,9]];
 
 function Board() {
   this.grid = [["","",""],["","",""],["","",""]];
+  this.turn = 0;
+  this.currentPlayer = 0;
+
 }
 Board.prototype.checkStatus = function() {
-  //check if the board has winning moves or a draw
+  //check if the board has winning moves
+  for (var j=0; j<3; j++) {
+    var xCount = 0;
+    var oCount = 0;
+    for (var i=0; i<3; i++){
+      if (board.grid[j][i] === "X"){
+        xCount += 1;
+        if (xCount === 3) {return "Player 1 wins"} //return something
+      } else if (board.grid[j][i] === "O") {
+        oCount += 1;
+        if (oCount === 3) {return "Player 2 wins"} //return something
+      }
+    }
+  }
   //Vertical
-    //along x
+  //along x
   //Horizontal
-    //along y
+  //along y
   //Diagonal
-    //x=y
-    //x+y=2
+  //x=y
+  //x+y=2
+  //Check Draw
 }
-function Player() {
-  this.mark = "";
+
+Board.prototype.advanceTurn = function() {
+  this.turn += 1
+  if (turn % 2 === 0) {       //Determine Player
+    this.currentPlayer = 1;
+  } else {
+    this.currentPlayer = 2
+  }
+  return
+}
+
+Board.prototype.displayBoard = function() {
+  //output board to html
+}
+
+function Player(id) {
+  this.id = id;
+  if (this.id === 1) {
+    this.mark = 'X';
+  } else {
+    this.mark = 'O'
+  }
   this.wins = 0;
   this.loss = 0;
+
 }
+
 Player.prototype.makeMove = function(x,y,board) {
-  if (board.grid[x][y])
-  board.grid[x][y] = this.mark;
+  if (!board.grid[x][y]) {
+    board.grid[x][y] = this.mark;
+    console.log('success')
+    board.advanceTurn(this);
+    return true;
+  } else {
+    //return an error and alert user
+    console.log('space taken')
+  }
 }
+
+Player.prototype.checkPlayer = function() {
+  return this.id;
+}
+
+//test objects
+testBoard = new Board();
+testPlayer = new Player(1);
+
 
 
 $(document).ready(function(){
+
+  if (testPlayer.makeMove())
+
 
 });
